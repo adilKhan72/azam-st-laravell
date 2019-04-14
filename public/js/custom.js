e5b774bad7f7c3e242726f9fec16fafc6fbb4297
adilkhan <adilkhanengineer72@gmail.com>
2019-04-14 14:54:06 +0500

    azam-st index with sources like css javascript added

---
+++ public/js/custom.js
@@ -0,0 +1,8 @@
+//coloring birds and making background transparent for animation of vanta.js
+		VANTA.BIRDS({
+			el: "#birds_animation",
+			backgroundColor: 0xbcc3be,
+			color2: 0xFF0D11,
+			color1: 0xFF0D11,
+			backgroundAlpha: 0.0
+		})
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,180 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Required meta tags -->
+  <meta charset="utf-8">
 
-        <title>Laravel</title>
+  <!-- browser will (probably) render the width of the page at the width of its own screen -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+  <!-- including custom styles -->
+  <link rel="stylesheet" type="text/css" href="{{asset('public/css/styles.css')}}">
+  <!-- Title  -->
+  <title>Azam systems & Technologies</title>
 
-            .full-height {
-                height: 100vh;
-            }
+</head> 
+<!-- body starts -->
+<body>
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top ">
+    <div class="container-fluid navbarcustom">
+      <a class="navbar-brand" href="#">
+        <img src="{{asset('public/images/logo.png')}}" alt="">
+      </a>
+      <!-- navbar responsive -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <!-- inline element -->
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <!-- navbar collapse on minimizing the screen -->
+      <div class="collapse navbar-collapse " id="navbarResponsive">
+        <!-- navbar alignment to auto -->
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <!-- Home button linked to home page -->
+            <a class="nav-link" href="#">HOME
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">ABOUT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">PORTFOLIO</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">SERVICES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">CONTACT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">ClIENTS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">CAREER</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!-- navbar ends here -->
+  </nav>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+<div>
+    <div id="birds_animation" class="birds_animation_cls">
+  </div>
+</div>
 
-            .position-ref {
-                position: relative;
-            }
+<div class="container-fluid"><!-- container start -->
+	<div class="row">
+		<div class="col">
+	<!-- div for birds animation after navbar -->
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+</div>
+</div>
+<div class="about_us">
+  <h1>ABOUT</h1>
+</div>
+<!-- About div in Grids starts here (First Row: 3 coloumns) -->
+<div class="row">
+  <div class="col-lg-4 ">
+    <h3 align="center">About Us</h3>
+    <p>Established in 2014, Azam Systems & Technologies (Pvt.) Limited is a leading software company providing affordable, reliable and user-friendly software solutions. Our approach is customer-centric and user experience is number one for us. We go to extraordinary lengths to satisfy, delight and nourish our customers.</p>
+  </div>
+  <div class="col-lg-4">
+    <h3 align="center">Our Vision</h3>
+    <p>
+    To become an accepted leader in software development and information technology and provide high quality people, products and services with a personal and professional touch.</p>
+  </div>
+  <div class="col-lg-4">
+    <h3 align="center">Our Mission</h3>
+    <p>Our mission is to help businesses and organizations make the best use of their resources, increase efficiency, reduce costs, improve profitability, enhance customer satisfaction and realize their full potential by providing them with best-of-breed information systems products and solutions.</p>
+  </div>
+  
+  <!-- First row ends here -->
+</div>
+</div>
+<!-- Services div -->
+<div class="services">
+  <h1>SERVICES</h1>
+</div>
+<!-- Services in Grids start (First Row: 4 coloumns) -->
+<div class="row">
+  <div class="col-lg-4 ">
+    <h4>Software Design & Development</h4>
+  </div>
+  <div class="col-lg-4">
+    <h4>Mobile Application Development</h4>
+  </div>
+  <div class="col-lg-4">
+    <h4>Website Design & Development</h4>
+  </div>
+  
+  <!-- First row ends here -->
+</div>
+<!-- 2nd row starts here -->
+<div class="row">
+  <div class="col-lg-4 ">
+    <h4>Business Analysis & IT Consulting</h4>
+  </div>
+  <div class="col-lg-4">
+    <h4>System Integration</h4>
+  </div>
+  <div class="col-lg-4">
+    <h4>IT Infrastructure Management</h4>
+  </div>
+  <!-- 2nd row ends here -->
+</div>
+<!-- projects div starts here -->
+<div class="projects">
+  <h1>Feature Projects</h1>
+</div>
+<!-- projects in Grids starts here (First Row: 4 coloumns) -->
+<div class="row">
+  <div class="col-lg-4 ">
+    <h4 >EIMS</h4>
+  </div>
+  <div class="col-lg-4">
+    <h4>AZAM PMS</h4>
+  </div>
+  <div class="col-lg-4">
+    <h4>jaidadtrade.com</h4>
+  </div>
+  
+  <!-- First row ends here -->
+</div>
+<!-- 2nd row starts here -->
+<div class="row">
+  <div class="col-lg-4 ">
+    <h4>Egetwork.com</h4>
+  </div>
+  <div class="col-lg-4">
+    <h4>Rizkdoor.com</h4>
+  </div>
+  <div class="col-lg-4">
+    <h4>View Other</h4>
+  </div>
+  <!-- 2nd row ends here -->
+</div>
 
-            .content {
-                text-align: center;
-            }
+</div><!-- container end -->
 
-            .title {
-                font-size: 84px;
-            }
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  	<!-- including js file for animation of vanta.js -->
+    <script src="{{asset('public/js/birds_animation/three.r92.min.js')}}"></script>
+	<!-- including js file for specifically brids animation of vanta.js -->
+	<script src="{{asset('public/js/birds_animation/vanta.birds.min.js')}}"></script>
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+  <script src="{{asset('public/js/custom.js')}}"></script>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Abdul waheed changed from my abdulwaheed laptop  
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="about">about</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+<!-- body ends here! -->
+</body>
 </html>
